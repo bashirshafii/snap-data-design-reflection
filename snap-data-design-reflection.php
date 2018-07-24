6b91dd9a0c68c2bc9d36700276df109869d80fbb
Bashir Shafii <bashir.shafii@gmail.com>
2018-07-24 09:47:04 -0500

    first commit

---
+++ snap-data-design-reflection.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Reflection</title>
+		<link rel="stylesheet" href="reflections.css">
+	</head>
+	<body>
+		<h1>Reflections</h1>
+		<h5>0. What is the most significant thing you learnt about object oriented?</h5>
+		<p>It's the foundation to building highly organized, scalable and maintainable application
+		code. </p>
+		<h5>1. What is the most significant thing you have learned about the effect of the frontend
+		on the backend?</h5>
+		<p>The backend has a 'uuuuuuuuuuuuge' impact on the design of the front. The workflow of the application
+		, and the user interaction is ultimately dependent on the backend. The front-end, while very important, is
+		ultimately dependent on the backend's functionality.</p>
+		<h5>2. Why is encapsulation so important?</h5>
+		<p>It helps us keep the data/state/properties of our classes private and access them only through mutator and accessor methods.
+		This has important security and maintainability implications.</p>
+		<h5>What is the air speed velocity of a laden swallow?</h5>
+		<p>3. What is a laden swallow?</p>
+		<?php
+
+		?>
+		
+	</body>
+</html>
